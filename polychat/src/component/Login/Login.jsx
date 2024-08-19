add58e39c050e19e5abaa5cbc9be94444511b00d
dokhung <94828988+dokhung@users.noreply.github.com>
2024-08-19 10:05:53 +0900

    background

--- polychat/src/component/Login/Login.jsx
+++ polychat/src/component/Login/Login.jsx
@@ -1,35 +1,27 @@
 import React from 'react';
 import './LoginStyle.css';
-import background from '../../ImgDocument/BackGround.jpg';
-import {Link} from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 export const Login = () => {
     return (
-        <div
-            className="LoginBackground"
-            style={{
-                backgroundImage: `url(${background})`,
-            }}
-        >
+        <div className="LoginBackground">
+            <div className="Earth"></div>
             <div className="LoginContainer">
-                <h1 className="TitleStyle">PolyChat</h1>
+                <div className="TitleStyle"></div>
                 <div>
-                    <h5 className={"LoginText"}>Account</h5>
+                    <h5 className="LoginText">Account</h5>
                     <input className="LoginInput" type="text" placeholder="Username" />
-                    <h5 className={"LoginText"}>PassWord</h5>
+                    <h5 className="LoginText">PassWord</h5>
                     <input className="LoginInput" type="password" placeholder="Password" />
-                    <div className={"ButtonContainer"}>
-                            <Link to={"/gamemain"}>
-                                <button className={"ButtonStyle"}>
-                                <h5 className={"LoginText"}>Login</h5>
-                                </button>
-                            </Link>
+                    <div className="ButtonContainer">
+                        <Link to={"/gamemain"}>
+                            <button className="ButtonStyle">
+                                <h5 className="LoginText">Login</h5>
+                            </button>
+                        </Link>
                     </div>
                 </div>
             </div>
         </div>
     );
 };
-
-
-
