2b4d6afe465b99a81fa02acc8a0ccb3986f5d0cb
dokhung <94828988+dokhung@users.noreply.github.com>
2024-10-21 22:44:09 +0900

    unity

--- polychat/src/component/unity/UnityComponent.jsx
+++ polychat/src/component/unity/UnityComponent.jsx
@@ -2,8 +2,8 @@ import { Unity, useUnityContext } from 'react-unity-webgl';
 import styled from 'styled-components';
 
 const FullScreenContainer = styled.div`
-  width: 100vw;   // 화면의 가로를 100%로 설정
-  height: 100vh;  // 화면의 세로를 100%로 설정
+  width: 100vw;
+  height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -12,11 +12,12 @@ const FullScreenContainer = styled.div`
 
 export const UnityComponent = () => {
     const { unityProvider } = useUnityContext({
-        loaderUrl: "Build/Documents.loader.js",
-        dataUrl: "Build/Documents.data",
-        frameworkUrl: "Build/Documents.framework.js",
-        codeUrl: "Build/Documents.wasm",
+        loaderUrl: "Build/Web.loader.js",
+        dataUrl: "Build/Web.data",
+        frameworkUrl: "Build/Web.framework.js",
+        codeUrl: "Build/Web.wasm",
     });
+
 
     return (
         <FullScreenContainer>
