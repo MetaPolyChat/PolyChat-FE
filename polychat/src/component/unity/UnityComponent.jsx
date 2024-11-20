7cc2d081acf3339ac4013305f4ec8e6c8c868329
dokhung <ehhiop123@naver.com>
2024-11-20 10:03:36 +0900

    fun

--- polychat/src/component/unity/UnityComponent.jsx
+++ polychat/src/component/unity/UnityComponent.jsx
@@ -1,6 +1,7 @@
 import { Unity, useUnityContext } from 'react-unity-webgl';
 import styled from 'styled-components';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const FullScreenContainer = styled.div`
     width: 100vw;
@@ -19,6 +20,18 @@ export const UnityComponent = ({ parameter }) => {
         codeUrl: "/Build/webgl.wasm",
     });
 
+    const [userData, setUserData] = useState(null);
+    useEffect(()=>{
+        axios.get('https://polychat.fun:18000/info?userId=11')
+            .then(res=>{
+                setUserData(res.data);
+                console.log("res.data :: " + res.data);
+            })
+            .catch(error=>{
+                console.log('Error떳다',error);
+            });
+    },[]);
+
     // Unity가 로드되면 자동으로 메시지 보내기
     useEffect(() => {
         if (isLoaded) {
@@ -27,6 +40,7 @@ export const UnityComponent = ({ parameter }) => {
             }, 100);
         }
     }, [isLoaded, sendMessage]);
+    
 
     return (
         <FullScreenContainer>
