05a30f3a1c15f98afed33501ad53bd608c9abc83
dokhung <ehhiop123@naver.com>
2024-11-20 15:45:31 +0900

    name

--- polychat/src/component/unity/UnityComponent.jsx
+++ polychat/src/component/unity/UnityComponent.jsx
@@ -13,36 +13,40 @@ const FullScreenContainer = styled.div`
 `;
 
 export const UnityComponent = ({ parameter }) => {
+    const searchParams = new URLSearchParams(location.search);
+    const userId = searchParams.get('userId');
+    const [userName, setUserName] = useState(null);
+    const [userData, setUserData] = useState(null);
     const { unityProvider, isLoaded, sendMessage } = useUnityContext({
         loaderUrl: "/Build/webgl.loader.js",
         dataUrl: "/Build/webgl.data",
         frameworkUrl: "/Build/webgl.framework.js",
         codeUrl: "/Build/webgl.wasm",
     });
-    const searchParams = new URLSearchParams(location.search);
-    const userId = searchParams.get('userId');
-    console.log("userId ::" +userId)
-    const [userData, setUserData] = useState(null);
-    useEffect(()=>{
+
+    useEffect(() => {
         axios.get(`https://polychat.fun:18000/api/info?userId=${userId}`)
             .then(res => {
-                console.log("res.data :: ", JSON.stringify(res.data, null, 2)); // Beautifies JSON output
+                console.log("res.data :: ", JSON.stringify(res.data, null, 2));
+                console.log("res.data.userName :: " + res.data.userName);
                 setUserData(res.data);
-                console.log("res.data :: ", JSON.stringify(res.data, null, 2)); // Beautifies JSON output
+                setUserName(res.data.userName);
             })
             .catch(error => {
                 console.log('Error 떳다', error);
             });
-    },[]);
+    }, [userId]);
 
-    // Unity가 로드되면 자동으로 메시지 보내기
+    // Unity가 로드되고 userName이 설정되면 메시지 보내기
     useEffect(() => {
-        if (isLoaded) {
+        if (isLoaded && userName) {
             setTimeout(() => {
-                sendMessage('LoginTestScript', 'RecieveUnity', setUserData);
+                sendMessage('LoginTestScript', 'RecieveUnity', userName);
             }, 100);
         }
-    }, [isLoaded, sendMessage]);
+    }, [isLoaded, sendMessage, userName]);
+    
+    
     
 
     return (
